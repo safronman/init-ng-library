791ee8fbab50dbe81f3e226ebef5626ffb4cac95
safronman <safronmanbox@gmail.com>
2021-07-15 14:42:25 +0300

    add lodash; add source-map explorer to analyze bundle size; bundle lodash size 69, 35 KB; check

--- projects/in-lib/src/lib/components/in-a/in-a.component.ts
+++ projects/in-lib/src/lib/components/in-a/in-a.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
+
 
 interface IArr {
   id: number
@@ -12,19 +14,24 @@ interface IArr {
 })
 export class InAComponent implements OnInit {
 
-  arr: IArr[] = []
+  arr: IArr[] = [
+    {id: 1, title: '1'},
+    {id: 2, title: '2'},
+    {id: 3, title: '3'},
+  ]
 
   constructor() {
   }
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.arr = [
-        {id: 1, title: '1'},
-        {id: 2, title: '2'},
-        {id: 3, title: '3'},
-      ]
-    }, 2000)
+
+    let arr2 = this.arr
+    console.log('must return true', arr2 === this.arr)
+
+    let deepArr = _.cloneDeep(this.arr);
+    console.log('must return false', deepArr === this.arr)
+
+
   }
 
 
